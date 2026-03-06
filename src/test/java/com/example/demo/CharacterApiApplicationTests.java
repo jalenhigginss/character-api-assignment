3435854a73c1d26454efe73acf490bd7fdd53493
Jalen <jalenhiggins2003@gmail.com>
2026-03-06 16:15:19 -0500

    Initial SpringBoot scaffolding

---
+++ src/test/java/com/example/demo/CharacterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
